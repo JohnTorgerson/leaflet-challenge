22a4afd09cb010fedbf997126b4bdd7c55fc7f00
John <107654605+JohnTorgerson@users.noreply.github.com>
2022-10-15 13:49:02 -0500

    added circles, fixed plates

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,103 +1,131 @@
+// Define earthquakes & tectonic plates from GeoJSON
+console.log("Escape From LA!!")
+
 // Store our API earthquakes endpoint as quakeUrl
-let quakeUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
-
+const quakeUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
 // Store our API tectonic plates endpoint as plateUrl
-let plateUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
+const plateUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
 
-// Perform a GET request to the query URL/
-d3.json(quakeUrl).then(function (data) {
-  // Once we get a response, send the data.features object to the createFeatures function.
-  console.log(data); 
-  createFeatures(data.features);
+// Pass base toggle on/off layers into variables
+let quakes = L.layerGroup();
+let tec_plates = L.layerGroup();
+
+// Pass base layer keys into object variable
+let baseMap = {
+    "Earthquakes": quakes,
+    "Tectonic Plates": tec_plates
+};
+
+// Pass tile layers select into variables
+var gray = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/light-v10",
+    accessToken: key
 });
 
-let markerSize = 1000; // Adjust by Magnitude
-let markerColor = "indigo"; // Adjust by Depth
+var dark = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: key
+});
 
-function createFeatures(earthquakeData) {
+var outdoor = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/outdoors-v11",
+    accessToken: key
+});
 
-  // Define a function that we want to run once for each feature in the features array.
-  // Give each feature a popup that describes the place and time of the earthquake.
-  function onEachFeature(feature, layer) {
-    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+var sat = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.satellite",
+    accessToken: key
+});
+
+// Pass tile layer keys into object varaible
+let tiledMap = {
+    "Satellite Map": sat,
+    "Grayscale Map": gray,
+    "Outdoors Map": outdoor,
+    "Dark Map": dark
+};
+
+// Create primary load map, using greyscale and earthquake layers
+let eqMap = L.map("map", {
+    center: [32, 10],
+    zoom: 2,
+    layers: [gray, quakes]
+});
+
+// Add a layer control to the earthquake map
+L.control.layers(tiledMap, baseMap, {
+    collapsed: false
+}).addTo(eqMap);
+
+// Set marker parameters from earthquake json 
+d3.json(quakeUrl, function(quakeData) {
+  
+  // Set marker size params by magnintude from earthquake json
+  function markerSize(magnitude) {
+      return magnitude * 2.25;
+  }; 
+
+  // Set marker color params by depth from earthquake json 
+  function chooseColor(depth) {
+      switch(true) {
+          case depth >90:
+              return "#3C391B";
+          case depth > 70:
+              return "#5A512A";
+          case depth > 50:
+              return "#766839";
+          case depth > 30:
+              return "927C49";
+          case depth > 10:
+              return "#9A704E";
+          default:
+              return "#A36153";
+      }
   }
 
-  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-  // Run the onEachFeature function once for each piece of data in the array.
-  let quake = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
+  // Create a GeoJSON layer containing the features array
+  L.geoJSON(quakeData, {
+      pointToLayer: function(feature, latlng) {
+          return L.circleMarker(latlng, {
+              radius: markerSize(feature.properties.mag),
+              fillColor: chooseColor(feature.geometry.coordinates[2]),
+              fillOpacity: 0.5,
+              color: "white",
+              stroke: false,
+              weight: 0.3
+          })
+      },
+      onEachFeature: function(feature, layer) {
+          layer.bindPopup("<h3>Location: " + feature.properties.place + "</h3><hr><p>Date: " + new Date(feature.properties.time) + "</p><hr><p>Magnitude: " + feature.properties.mag + "</p>");
+      }
+  }).addTo(quakes);
+  quakes.addTo(eqMap);
+  console.log(quakeData);
+
+  // Set marker parameters from earthquake json 
+  d3.json(plateUrl, function(data) {
+      L.geoJSON(data, {
+          color: "blue",
+          opacity: .5, 
+          weight: 1
+      }).addTo(tec_plates);
+      tec_plates.addTo(eqMap);
+      console.log(data); 
   });
-
-  // Send our earthquakes layer to the createMap function/
-  createMap(quake);
-}
-
-function createMap(earthquakes) {
-
-  // Create the base layers.
-  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  })
-
-  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-  });
-
-  // Create a baseMaps object.
-  let baseMaps = {
-    "Street Map": street,
-    "Topographic Map": topo
-  };
-
-  // Create an overlay object to hold our overlay.
-  let overlayMaps = {
-    // "Tectonic Plates": tecplates,
-    "Earthquakes": earthquakes
-  };
-
-  // Create our map, giving it the streetmap and earthquakes layers to display on load.
-  let myMap = L.map("map", {
-    center: [
-      32.000, 10.000
-    ],
-    zoom: 1,
-    layers: [street, earthquakes]
-  });
-
-  // Create a layer control.
-  // Pass it our baseMaps and overlayMaps.
-  // Add the layer control to the map.
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
-
-}
-
-
   
-//   function createMarkers(response) {
-  
-//     // Pull the "station"**Might Need to Change** property from response.data.
-//     var station = response.data.station;
-  
-//     // Initialize an array to hold earthquake circles.
-//     var quakeCenters = [];
-  
-//     // Loop through the station array.
-//     for (var i = 0; i < station.length; i++) {
-//       var station = station[i];
-  
-//       // For each station, create a marker, and bind a popup with the station's name.
-//       var quakeCircle = L.circle([station.lat, station.lon], {
-//         stroke: false,
-//         fillOpacity: 0.5,
-//         color: "white",
-//         fillColor: "blue",
-//         // set radius to magnitude
-//         radius: circleRad(station[i].capacity)
-//       }).bindPopup("<h3>" + station.name + "<h3><h3>Capacity: " + station.capacity + "</h3>");
-  
-//       // Add the marker to the quakeCenters array.
-//       quakeCenters.push(quakeCircle);
-//     }
-  
+  // Create Legend for color-depth chart
+  // if time allows
+
+});
